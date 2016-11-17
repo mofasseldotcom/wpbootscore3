36613862728306453cf276a9d0d4a97eee187027
webguymofassel <webguymofassel@gmail.com>
2016-11-17 12:37:35 +0600

    bootstrap css, js, navwalker added, nav dynamic

--- functions.php
+++ functions.php
@@ -104,6 +104,11 @@ add_action( 'widgets_init', 'wpbootscore3_widgets_init' );
 function wpbootscore3_scripts() {
 	wp_enqueue_style( 'wpbootscore3-style', get_stylesheet_uri() );
 
+	wp_enqueue_style('wpbootscore3-bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css');
+	
+
+	wp_enqueue_script( 'wpbootscore3-bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '3.3.7', true );
+
 	wp_enqueue_script( 'wpbootscore3-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'wpbootscore3-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
@@ -138,3 +143,8 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+/**
+ * // Register Custom Navigation Walker
+ */
+require get_template_directory() . '/inc/wp_bootstrap_navwalker.php';
--- header.php
+++ header.php
@@ -38,9 +38,35 @@
 			endif; ?>
 		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'wpbootscore3' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+		<nav id="site-navigation" class="main-navigation navbar navbar-default" role="navigation">
+		  <div class="container">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#primary-navbar-collapse">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <a class="navbar-brand" href="<?php echo home_url(); ?>">
+		                <?php bloginfo('name'); ?>
+		            </a>
+		    </div>
+
+		        <?php
+		            wp_nav_menu( array(
+		                'menu'              => 'primary',
+		                'theme_location'    => 'primary',
+		                'depth'             => 2,
+		                'container'         => 'div',
+		                'container_class'   => 'collapse navbar-collapse',
+		        'container_id'      => 'primary-navbar-collapse',
+		                'menu_class'        => 'nav navbar-nav navbar-right',
+		                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+		                'walker'            => new wp_bootstrap_navwalker())
+		            );
+		        ?>
+		    </div>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
